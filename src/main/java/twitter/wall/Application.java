4ff2b385ab1d1c14209605aadc5120a651fe0c72
Yevhenii Melnyk <melnyk.yevhenii@gmail.com>
2017-03-20 23:08:40 +0200

    Gradle bower dependencies
    Sample js/java event bus message

--- src/main/java/twitter/wall/Application.java
+++ src/main/java/twitter/wall/Application.java
@@ -3,6 +3,7 @@ package twitter.wall;
 import io.vertx.core.Vertx;
 import twitter.wall.verticle.Verticle;
 
+// To run from idea
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/twitter/wall/verticle/Verticle.java
+++ src/main/java/twitter/wall/verticle/Verticle.java
@@ -1,11 +1,15 @@
 package twitter.wall.verticle;
 
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandlerOptions;
 import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.ext.web.Route;
 import io.vertx.rxjava.ext.web.Router;
 import io.vertx.rxjava.ext.web.handler.StaticHandler;
 import io.vertx.rxjava.ext.web.handler.sockjs.SockJSHandler;
-import rx.Observable;
-import rx.Subscription;
+
+import static java.util.stream.Collectors.toList;
 
 public class Verticle extends AbstractVerticle {
 
@@ -14,22 +18,22 @@ public class Verticle extends AbstractVerticle {
 	public void start() throws Exception {
 		Router router = Router.router(vertx);
 
-		router.route("/news-feed/*").handler(SockJSHandler.create(vertx).socketHandler(sockJSSocket -> {
-			Observable<String> msg = vertx.eventBus().<String>consumer("news-feed")
-					.bodyStream()
-					.toObservable();
-
-			Subscription subscription = msg.subscribe(sockJSSocket::write);
-
-			sockJSSocket.endHandler(v -> subscription.unsubscribe());
-		}));
-
+		BridgeOptions bridgeOptions = new BridgeOptions()
+				.addOutboundPermitted(new PermittedOptions().setAddress("some-address"))
+				.addInboundPermitted(new PermittedOptions().setAddress("some-address"));
+		SockJSHandler sockJsHandler = SockJSHandler.create(vertx).bridge(bridgeOptions, be -> {
+			System.out.println("EVENT: " + be.type());
+			be.complete(true);
+		});
+		router.route("/feed/*").handler(sockJsHandler);
+		router.route("/assets/*").handler(StaticHandler.create("assets"));
 		router.route().handler(StaticHandler.create());
-
 		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 
-		vertx.setPeriodic(1000, t -> vertx.eventBus().publish("news-feed", "news from the server!"));
-
+		vertx.eventBus().consumer("some-address", m -> {
+			System.out.println("GOT MESSAGE:" + m.address() + ":" + m.replyAddress() + "  " + m.body());
+		});
+		vertx.setPeriodic(3000, t -> vertx.eventBus().publish("news-feed", "news from the server!"));
 
 	}
 
